2fc442f6d66bf6bf082a1fccfa71905b4887c556
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2020-03-22 20:19:52 +0100

    order functions

--- src/server/Domain/Model/Harvest/HarvestId.ts
+++ src/server/Domain/Model/Harvest/HarvestId.ts
@@ -3,16 +3,16 @@ import { v4 as uuidv4 } from 'uuid';
 export class HarvestId {
   _id: String
 
+  static create() {
+    return new HarvestId(uuidv4());
+  }
+
   constructor(id: String) {
     this._id = id;
   }
 
   id() {
     return this._id;
-  }
-
-  static create() {
-    return new HarvestId(uuidv4());
   }
 
   equalTo(aHarvestId: HarvestId) {
