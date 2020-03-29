ab157ac8b8174dde92b899801402ca333c496299
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2020-03-29 19:21:25 +0200

    add parcel and react

--- src/server/Domain/Model/Harvest/HarvestRepository.ts
+++ src/server/Domain/Model/Harvest/HarvestRepository.ts
@@ -4,9 +4,9 @@ import { Harvest } from './Harvest';
 import { HarvestId } from './HarvestId';
 
 export interface HarvestRepository {
-    nextIdentity: () => HarvestId;
-    add: (harvest: Harvest) => void;
-    count: () => number;
-    filterBy: ({ date, location }: { date?: Date; location?: Location }) => Array<Harvest>;
-    findById: (id: HarvestId) => Harvest | undefined;
+  nextIdentity: () => HarvestId;
+  add: (harvest: Harvest) => void;
+  count: () => number;
+  filterBy: ({ date, location }: { date?: Date; location?: Location }) => Array<Harvest>;
+  findById: (id: HarvestId) => Harvest | undefined;
 }
