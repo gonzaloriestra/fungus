d28365658d289fb0869ea835e053db54993b509e
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2020-04-05 19:34:12 +0200

    include react

---
+++ client/App.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { Main } from './src/Main';
+
+ReactDOM.render(React.createElement(Main, { app: this }), document.getElementById('app'));
---
+++ client/src/components/Hello.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+
+export const Hello = (): JSX.Element => <div>Hello World</div>;
