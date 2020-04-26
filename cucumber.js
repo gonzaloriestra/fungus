ec228ae2ad3040a6482bfe0b429ec7ea1c03ba43
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2020-04-26 13:26:39 +0200

    improvement with cucumber

--- cucumber.js
+++ cucumber.js
@@ -1,6 +1,7 @@
 const common = [
-  'tests/**/*.feature', // Specify our feature files
-  '--require tests/**/*.js', // Load step definitions
+  'tests/**/features/**/*.feature', // Specify our feature files
+  '--require-module ts-node/register', // Load TypeScript module
+  '--require tests/**/features/**/step_definitions/*.steps.ts', // Load step definitions
   '--format progress-bar', // Load custom formatter
   '--format node_modules/cucumber-pretty', // Load custom formatter
 ].join(' ');
---
+++ tests/applications/fungus/backend/features/step_definitions/controller.steps.ts
@@ -0,0 +1,37 @@
+import assert from 'assert';
+import { AfterAll, Before, Given, Then } from 'cucumber';
+import request from 'supertest';
+import app from '../../../../../../applications/fungus/backend/app';
+// import container from '../../../../../src/apps/mooc_backend/config/dependency-injection';
+// import { EnvironmentArranger } from '../../../../Contexts/Shared/infrastructure/arranger/EnvironmentArranger';
+
+let _request: request.Test;
+let _response: request.Response;
+
+Given('I send a GET request to {string}', (route: string) => {
+  _request = request(app).get(route);
+});
+
+Given('I send a PUT request to {string} with body:', (route: string, body: string) => {
+  _request = request(app)
+      .put(route)
+      .send(JSON.parse(body));
+});
+
+Then('the response status code should be {int}', async (status: number) => {
+  _response = await _request.expect(status);
+});
+
+Then('the response should be empty', () => {
+  assert.deepEqual(_response.body, {});
+});
+
+// Before(async () => {
+//   const environmentArranger: Promise<EnvironmentArranger> = container.get('Mooc.EnvironmentArranger');
+//   await (await environmentArranger).arrange();
+// });
+
+// AfterAll(async () => {
+//   const environmentArranger: Promise<EnvironmentArranger> = container.get('Mooc.EnvironmentArranger');
+//   await (await environmentArranger).close();
+// });
