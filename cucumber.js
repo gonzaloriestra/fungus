1476795df406b36b17a5f2768b7e5d190f4a69f4
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2020-04-26 18:27:30 +0200

    remove cucumber progress bar

--- cucumber.js
+++ cucumber.js
@@ -2,7 +2,6 @@ const common = [
   'tests/**/features/**/*.feature', // Specify our feature files
   '--require-module ts-node/register', // Load TypeScript module
   '--require tests/**/features/**/step_definitions/*.steps.ts', // Load step definitions
-  '--format progress-bar', // Load custom formatter
   '--format node_modules/cucumber-pretty', // Load custom formatter
 ].join(' ');
 
