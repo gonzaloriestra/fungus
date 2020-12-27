12c20ebf5e566f5515c6e241fb9d47feb35af851
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2020-12-27 21:19:26 +0100

    remove harcoded mushrooms

--- src/Fungus/Mushrooms/Infrastructure/InMemoryMushroomRepository.ts
+++ src/Fungus/Mushrooms/Infrastructure/InMemoryMushroomRepository.ts
@@ -4,18 +4,10 @@ import { MushroomRepository } from '../Domain/MushroomRepository';
 import { Mushroom } from '../Domain/Mushroom';
 import { Mushrooms } from '../Domain/Mushrooms';
 
-// To-Do this is temporal, so we should remove it
-const initialMushrooms = [
-  new Mushroom({ id: new MushroomId('9f201a5a-4cbb-427c-935d-0a52b0bf4742'), scientificName: 'Boletus Edulis' }),
-  new Mushroom({ id: new MushroomId('0ce9affe-79b3-473c-9c0d-31976d04e149'), scientificName: 'Amanita Caesarea' }),
-  new Mushroom({ id: new MushroomId('a8c15ce6-2310-4572-a3f7-b13a8900d483'), scientificName: 'Boletus Aereus' }),
-  new Mushroom({ id: new MushroomId('84270069-10a2-4c52-80be-9dd8a70f47a3'), scientificName: 'Amanita Muscaria' }),
-];
-
 export class InMemoryMushroomRepository implements MushroomRepository {
   private _mushrooms: Mushrooms;
 
-  constructor({ mushrooms = new Mushrooms({ mushrooms: initialMushrooms }) }: { mushrooms?: Mushrooms } = {}) {
+  constructor({ mushrooms = new Mushrooms() }: { mushrooms?: Mushrooms } = {}) {
     this._mushrooms = mushrooms;
   }
 
