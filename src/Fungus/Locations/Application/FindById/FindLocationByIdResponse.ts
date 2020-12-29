479f69104b7fb3098d11becabd221f9212a24599
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2020-12-29 22:28:06 +0100

    clean response

--- src/Fungus/Locations/Application/FindById/FindLocationByIdResponse.ts
+++ src/Fungus/Locations/Application/FindById/FindLocationByIdResponse.ts
@@ -1,16 +1,9 @@
-import { Location } from '../../Domain/Location';
+import { Primitives } from '../../Domain/Location';
 
 export class FindLocationByIdResponse {
-  readonly id: string;
-  readonly name: string;
-  readonly zone: {
-    midpoint: { latitude: number; longitude: number };
-    coordinates: Array<{ latitude: number; longitude: number }>;
-  };
+  readonly data: Primitives;
 
-  constructor(location: Location) {
-    this.id = location.id().value();
-    this.name = location.name();
-    this.zone = { midpoint: location.zone().midpoint().toPrimitives(), coordinates: location.zone().toPrimitives() };
+  constructor(location: Primitives) {
+    this.data = location;
   }
 }
--- src/Fungus/Locations/Application/FindById/LocationByIdFinder.ts
+++ src/Fungus/Locations/Application/FindById/LocationByIdFinder.ts
@@ -18,6 +18,6 @@ export default class LocationByIdFinder {
       throw new LocationDoesNotExist(locationId);
     }
 
-    return new FindLocationByIdResponse(location);
+    return new FindLocationByIdResponse(location.toPrimitives());
   }
 }
