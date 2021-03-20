649968093b02b4eed405563c78e0037155d61ab4
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2021-03-20 20:28:16 +0100

    homepage style

--- applications/frontend/src/pages/index.tsx
+++ applications/frontend/src/pages/index.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
-import { Header, Icon } from 'semantic-ui-react';
-import { useUser } from '@auth0/nextjs-auth0';
-import AuthenticationButton from '../authentication/components/AuthenticationButton';
+import { Header, Icon, Container, Button, Segment, Grid } from 'semantic-ui-react';
+
+const HomepageHeading = (): JSX.Element => (
+  <Container text>
+    <Header as="h1" inverted style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}>
+      Fungus
+      <Icon name="detective" />
+    </Header>
+    <Header
+      as="h2"
+      content="Encuentra setas antes que los demás."
+      inverted
+      style={{ fontSize: '1.7em', fontWeight: 'normal', marginTop: '1.5em' }}
+    />
+    <Button href="/locations" primary size="huge">
+      Comenzar
+      <Icon name="right arrow" />
+    </Button>
+  </Container>
+);
+
+const HomepageContent = (): JSX.Element => (
+  <Grid>
+    <Grid.Row columns={3}>
+      <Grid.Column style={{ padding: '5em' }}>
+        <Icon circular color="olive" name="map" size="massive" />
+        <p style={{ fontSize: '1.33em', paddingTop: '1.5em' }}>Registra tus zonas de recolecta.</p>
+      </Grid.Column>
+      <Grid.Column style={{ padding: '5em' }}>
+        <Icon circular color="olive" name="rain" size="massive" />
+        <p style={{ fontSize: '1.33em', paddingTop: '1.5em' }}>Pidenos una predicción para una de tus zonas.</p>
+      </Grid.Column>
+      <Grid.Column style={{ padding: '5em' }}>
+        <Icon circular color="olive" name="hand victory" size="massive" />
+        <p style={{ fontSize: '1.33em', paddingTop: '1.5em' }}>Sal a recolectas con exito asegurado.</p>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
 
 export default function Home(): JSX.Element {
-  const { user } = useUser();
-
   return (
     <div>
       <Head>
@@ -16,17 +49,12 @@ export default function Home(): JSX.Element {
       </Head>
 
       <main>
-        <Header as="h1">
-          <Icon name="hand peace outline" />
-          <Header.Content>Fungus. {user ? `Welcome ${user.nickname}` : ''}</Header.Content>
-          <AuthenticationButton />
-        </Header>
-        <Header as="h2">
-          <Link href="/locations">My Locations</Link>
-        </Header>
-        <Header as="h2">
-          <Link href="/predictions">Make predictions</Link>
-        </Header>
+        <Segment inverted textAlign="center" style={{ minHeight: 600, padding: '1em 0em' }} vertical>
+          <HomepageHeading />
+        </Segment>
+        <Segment textAlign="center" style={{ minHeight: 500, padding: '1em 0em' }} vertical>
+          <HomepageContent />
+        </Segment>
       </main>
     </div>
   );
--- applications/frontend/src/pages/locations/index.tsx
+++ applications/frontend/src/pages/locations/index.tsx
@@ -9,9 +9,12 @@ import { withClientAuthRequired } from '../../authentication/withAuthRequired';
 import Location from './models/Location';
 
 import Item from './components/Item';
+import { useUser } from '@auth0/nextjs-auth0';
 
 function Locations(): JSX.Element {
   const result = useSWR(`/api/me/locations`, fetcher);
+
+  const { user } = useUser();
 
   const locations: Array<Location> = result.data;
   const error: Error = result.error;
@@ -29,6 +32,10 @@ function Locations(): JSX.Element {
       <Header as="h1">
         <Icon name="map outline" />
         <Header.Content>Locations</Header.Content>
+        <Header.Content>Welcome {user ? ` ${user.nickname}` : ''}</Header.Content>
+      </Header>
+      <Header as="h2">
+        <Link href="/predictions">Make predictions</Link>
       </Header>
       <Container>
         <Link href="/locations/new">
