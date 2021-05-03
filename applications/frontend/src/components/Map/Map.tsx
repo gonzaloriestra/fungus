95881b10fc1ec9782776251a9da0794f87101e00
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2021-05-03 16:31:39 +0200

    fix add location

--- applications/frontend/src/components/Map/Map.tsx
+++ applications/frontend/src/components/Map/Map.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { Map as MapLeaflet, WMSTileLayer, TileLayer, Polygon, FeatureGroup } from 'react-leaflet';
-import { EditControl } from 'react-leaflet-draw';
+import { Map as MapLeaflet, WMSTileLayer, TileLayer, Polygon } from 'react-leaflet';
 
 import Location from '../../models/Location';
 
 import styles from './map.module.css';
 import Coordinate from '../../models/Coordinate';
+import MapControls from './MapControls';
 
 type MapProps = {
   mode?: string;
@@ -30,57 +30,6 @@ const Map = ({
 
   const [position] = useState(initialPosition);
   const [zoom] = useState(initialZoom);
-  const [hasLocationDefined, setHasLocationDefined] = useState(mode === 'view');
-
-  const renderEditionMode = (mode): JSX.Element => {
-    if (mode !== 'edit') {
-      return null;
-    }
-    const handleOnCreate = (event): void => {
-      if (event.layer?.editing?.latlngs[0][0]) {
-        const coordinates = event.layer?.editing?.latlngs[0][0];
-
-        setHasLocationDefined(true);
-
-        onLocationCreated(coordinates.map((coordinate) => ({ latitude: coordinate.lat, longitude: coordinate.lng })));
-      }
-    };
-
-    const handleOnDelete = (event): void => {
-      if (event.layers?.getLayers().length > 0) {
-        setHasLocationDefined(false);
-      }
-    };
-
-    return (
-      <FeatureGroup>
-        <EditControl
-          position="topright"
-          draw={{
-            rectangle: false,
-            circle: false,
-            marker: false,
-            circlemarker: false,
-            polyline: false,
-            polygon: hasLocationDefined
-              ? false
-              : {
-                  allowIntersection: false,
-                  drawError: {
-                    color: '#e1e100',
-                    message: 'Oops! you cannot draw in that way.',
-                  },
-                  shapeOptions: {
-                    color: '#b5cc18',
-                  },
-                },
-          }}
-          onCreated={handleOnCreate}
-          onDeleted={handleOnDelete}
-        />
-      </FeatureGroup>
-    );
-  };
 
   const renderLocation = (location): JSX.Element => {
     if (!location) {
@@ -96,7 +45,7 @@ const Map = ({
     <MapLeaflet className={styles.container} center={position} zoom={zoom}>
       <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
       <WMSTileLayer url="https://www.ign.es/wms-inspire/ign-base" format="image/png" layers="IGNBaseOrto" transparent />
-      {renderEditionMode(mode)}
+      <MapControls mode={mode} onLocationCreated={onLocationCreated} />
       {renderLocation(location)}
     </MapLeaflet>
   );
