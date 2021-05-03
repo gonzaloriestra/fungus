01d049eec65073b1d13165dec0ac229f41efdcc3
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2021-05-03 20:24:50 +0200

    useHarvestsByLocationId now with SWR

--- applications/frontend/src/queries/getHarvestsByLocationId.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-type QueryResponse = {
-  data: object;
-};
-
-export default function getHarvestsByLocationId({ locationId }): Promise<QueryResponse> {
-  return axios.get(`http://localhost:3001/locations/${locationId}/harvests`);
-}
---
+++ applications/frontend/src/queries/useHarvestsByLocation.ts
@@ -0,0 +1,17 @@
+import useSWR from 'swr';
+
+import Harvest from '../models/Harvest';
+
+import fetcher from './fetcher';
+
+export default function getHarvestsByLocationId({
+  locationId,
+}): { harvests?: Array<Harvest>; isLoading: boolean; error: Error } {
+  const { data, error } = useSWR(`/api/harvests?locationId=${locationId}`, fetcher);
+
+  return {
+    harvests: data,
+    isLoading: !error && !data,
+    error,
+  };
+}
