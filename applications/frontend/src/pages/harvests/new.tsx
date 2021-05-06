10ef9e065747d866c7c485f0a41c572ec335925b
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2021-05-07 00:14:30 +0200

    reuse SelectorMushroom component

--- applications/frontend/src/pages/harvests/new.tsx
+++ applications/frontend/src/pages/harvests/new.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
-import { Header, Icon, Form, Button, Select } from 'semantic-ui-react';
+import { Header, Icon, Form, Button } from 'semantic-ui-react';
 import { useRouter } from 'next/router';
 
 import { withClientAuthRequired } from '../../authentication/withAuthRequired';
 
 import addHarvest from '../../actions/commands/addHarvest';
-import getMushrooms from '../../actions/queries/getMushrooms';
+import MushroomsSelector from '../../components/MushroomsSelector';
 
 function NewHarvest(): JSX.Element {
-  const { mushrooms, isLoading, error } = getMushrooms();
   const [date, setDate] = useState('');
   const [mushroomId, setMushroomId] = useState('');
   const [quantity, setQuantity] = useState('');
@@ -22,18 +21,6 @@ function NewHarvest(): JSX.Element {
 
     router.push(`/locations/${locationId}`);
   };
-
-  function transformMushroomsInOptions() {
-    return mushrooms.map((mushroom) => ({ key: mushroom.id, value: mushroom.id, text: mushroom.scientificName }));
-  }
-
-  if (error) {
-    return <div>Loading failed: {error.message}</div>;
-  }
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <>
@@ -53,13 +40,7 @@ function NewHarvest(): JSX.Element {
           <input type="date" placeholder="Date" onChange={(e): void => setDate(e.target.value)} />
         </Form.Field>
         <Form.Field>
-          <label>Mushroom</label>
-          <Select
-            placeholder="Mushroom"
-            options={transformMushroomsInOptions()}
-            // @ts-ignore
-            onChange={(_, data: { value: string }): void => setMushroomId(data.value)}
-          />
+          <MushroomsSelector onSelect={setMushroomId} />
         </Form.Field>
         <Form.Field>
           <label>Quantity</label>
