d61267ca35fd867bca5f17b3cd46a13e349d64fb
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2022-01-09 23:24:03 +0100

    remove locations controllers

--- applications/frontend/src/pages/api/locations/[id].ts
+++ applications/frontend/src/pages/api/locations/[id].ts
@@ -3,9 +3,15 @@ import httpStatus from 'http-status';
 
 import { LocationId } from '../../../Fungus/Shared/Domain/LocationId';
 import { UserId } from '../../../Fungus/Shared/Domain/UserId';
+import { WeatherStationId } from '../../../Fungus/Shared/Domain/WeatherStationId';
+import { Coordinate } from '../../../Fungus/Shared/Domain/Coordinate';
+import { LocationDoesNotExist } from '../../../Fungus/Shared/Domain/LocationDoesNotExist';
 
 import locationsByIdFinder from '../../../Fungus/Locations/Application/FindById';
 import locationCreator from '../../../Fungus/Locations/Application/Create';
+
+import { LocationAlreadyExist } from '../../../Fungus/Locations/Domain/LocationAlreadyExist';
+import { Zone } from '../../../Fungus/Locations/Domain/Zone';
 
 export default withApiAuthRequired(async function locations(req, res) {
   try {
@@ -19,12 +25,13 @@ export default withApiAuthRequired(async function locations(req, res) {
     } else {
       const { user } = getSession(req, res);
       const userId = user['http://fungus/user_id'];
-      const { name, zone } = JSON.parse(req.body);
+      const { name, coordinates, weatherStationId } = JSON.parse(req.body);
 
       await locationCreator.run({
         id: new LocationId(locationId),
         name,
-        zone,
+        zone: new Zone({ coordinates: coordinates.map((coordinate) => new Coordinate(coordinate)) }),
+        weatherStationId: weatherStationId ? new WeatherStationId(weatherStationId) : undefined,
         userId: new UserId(userId),
       });
 
@@ -32,6 +39,14 @@ export default withApiAuthRequired(async function locations(req, res) {
     }
   } catch (error) {
     console.error(error.message);
+    // ToDo avoid this defining the exception with and error code ???
+    if (error instanceof LocationDoesNotExist) {
+      return res.status(httpStatus.NOT_FOUND).end(error.message);
+    }
+    // ToDo avoid this defining the exception with and error code ???
+    if (error instanceof LocationAlreadyExist) {
+      return res.status(httpStatus.BAD_REQUEST).end(error.message);
+    }
 
     return res.status(error.status || httpStatus.INTERNAL_SERVER_ERROR).end(error.message);
   }
